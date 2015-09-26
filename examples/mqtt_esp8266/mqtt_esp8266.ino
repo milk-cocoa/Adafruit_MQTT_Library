6012295f7cc0e5420ed279e9c46d447dac2723c6
Kiyohiko Heima <piyo326@gmail.com>
2015-09-27 01:06:25 +0900

    Fix example for Milkcocoa

--- examples/mqtt_esp8266/mqtt_esp8266.ino
+++ examples/mqtt_esp8266/mqtt_esp8266.ino
@@ -1,34 +1,28 @@
 /***************************************************
   Adafruit MQTT Library ESP8266 Example
-
   Must use ESP8266 Arduino from:
     https://github.com/esp8266/Arduino
-
-  Works great with Adafruit's Huzzah ESP board:
-  ----> https://www.adafruit.com/product/2471
-
   Adafruit invests time and resources providing this open source code,
   please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
-
   Written by Tony DiCola for Adafruit Industries.
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 #include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
 
 /************************* WiFi Access Point *********************************/
 
-#define WLAN_SSID       "...your SSID..."
-#define WLAN_PASS       "...your password..."
+#define WLAN_SSID       "your_SSID"
+#define WLAN_PASS       "your_password"
 
-/************************* Adafruit.io Setup *********************************/
+/************************* Milkcocoa Setup *********************************/
 
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "...your AIO username (see https://accounts.adafruit.com)..."
-#define AIO_KEY         "...your AIO key..."
+#define MILKCOCOA_SERVER      "your_milkcocoa_app_id.mlkcca.com"
+#define MILKCOCOA_SERVERPORT  1883
+#define MILKCOCOA_USERNAME    "sdammy"
+#define MILKCOCOA_KEY         "your_milkcocoa_app_id"
 
 /************ Global State (you don't need to change this!) ******************/
 
@@ -37,27 +31,25 @@ WiFiClient client;
 
 // Store the MQTT server, client ID, username, and password in flash memory.
 // This is required for using the Adafruit MQTT library.
-const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
-// Set a unique MQTT client ID using the AIO key + the date and time the sketch
+const char MQTT_SERVER[] PROGMEM    = MILKCOCOA_SERVER;
+// Set a unique MQTT client ID using the Milkcocoa key + the date and time the sketch
 // was compiled (so this should be unique across multiple devices for a user,
 // alternatively you can manually set this to a GUID or other random value).
-const char MQTT_CLIENTID[] PROGMEM  = __TIME__ AIO_USERNAME;
-const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
-const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+const char MQTT_CLIENTID[] PROGMEM  = __TIME__ MILKCOCOA_USERNAME;
+const char MQTT_USERNAME[] PROGMEM  = MILKCOCOA_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = MILKCOCOA_KEY;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MILKCOCOA_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
 
-/****************************** Feeds ***************************************/
+/****************************** Topic ***************************************/
 
-// Setup a feed called 'photocell' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-const char PHOTOCELL_FEED[] PROGMEM = AIO_USERNAME "/feeds/photocell";
-Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, PHOTOCELL_FEED);
-
-// Setup a feed called 'onoff' for subscribing to changes.
-const char ONOFF_FEED[] PROGMEM = AIO_USERNAME "/feeds/onoff";
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, ONOFF_FEED);
+// Setup a pubulisher & subscriber.
+// Topic Name: "your_milkcocoa_app_id/your_milkcocoa_datastore_name/method_name"
+const char PUSH_TOPIC[] PROGMEM = "your_milkcocoa_app_id/your_milkcocoa_datastore_name/push";
+const char SEND_TOPIC[] PROGMEM = "your_milkcocoa_app_id/your_milkcocoa_datastore_name/send";
+Adafruit_MQTT_Publish pushPublisher = Adafruit_MQTT_Publish(&mqtt, PUSH_TOPIC);
+Adafruit_MQTT_Subscribe pushSubscriber = Adafruit_MQTT_Subscribe(&mqtt, PUSH_TOPIC);
 
 /*************************** Sketch Code ************************************/
 
@@ -83,7 +75,7 @@ void setup() {
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
 
   // Setup MQTT subscription for onoff feed.
-  mqtt.subscribe(&onoffbutton);
+  mqtt.subscribe(&pushSubscriber);
 }
 
 uint32_t x=0;
@@ -94,32 +86,18 @@ void loop() {
   // function definition further below.
   MQTT_connect();
 
-  // Try to ping the MQTT server
-  /*
-  if (! mqtt.ping(3) ) {
-    // MQTT pings failed, lets reconnect
-    Serial.println("Ping fail!");
-  }
-  */
-
   // this is our 'wait for incoming subscription packets' busy subloop
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(1000))) {
-    if (subscription == &onoffbutton) {
-      Serial.print(F("Got: "));
-      Serial.println((char *)onoffbutton.lastread);
-    }
-  }
+ Adafruit_MQTT_Subscribe *subscription;
+ while ((subscription = mqtt.readSubscription(1000))) {
+   if (subscription == &pushSubscriber) {
+     Serial.print(F("Got: "));
+     Serial.println((char *)pushSubscriber.lastread);
+   }
+ }
 
-  // Now we can publish stuff!
-  Serial.print(F("\nSending photocell val "));
-  Serial.print(x);
-  Serial.print("...");
-  if (! photocell.publish(x++)) {
-    Serial.println(F("Failed"));
-  } else {
-    Serial.println(F("OK!"));
-  }
+  // publish
+  // e.g. publish("{ \"params\":{ \"your_key1\":\"your_value1\", \"your_key2\":\"your_value2\",...} }");
+  pushPublisher.publish("{\"params\":{\"content\":\"from ESP8266!\"}}");
 
   delay(1000);
 }
@@ -138,6 +116,7 @@ void MQTT_connect() {
 
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
        Serial.println(mqtt.connectErrorString(ret));
+       Serial.println(ret);
        Serial.println("Retrying MQTT connection in 5 seconds...");
        mqtt.disconnect();
        delay(5000);  // wait 5 seconds
